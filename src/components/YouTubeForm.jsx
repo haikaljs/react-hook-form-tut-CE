83aa4a2710f9ecbb411440c8c80bd93a2ce53add
haikaljs <haikaljavascript@gmail.com>
2023-07-21 23:21:22 +0800

    Install devtools for react-hook-form

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const YouTubeForm = () => {
   const form = useForm();
-  const { register } = form;
+  const { register, control } = form;
   //   const { name, ref, onChange, onBlur } = register("username");
 
   return (
@@ -28,6 +29,7 @@ const YouTubeForm = () => {
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
