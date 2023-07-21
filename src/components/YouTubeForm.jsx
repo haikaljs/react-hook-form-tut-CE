c414fdde961f0f935536d4fd280fb44c04857389
haikaljs <haikaljavascript@gmail.com>
2023-07-21 23:25:07 +0800

    Form state and rerenders

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+let renderCount = 0;
 const YouTubeForm = () => {
   const form = useForm();
   const { register, control } = form;
   //   const { name, ref, onChange, onBlur } = register("username");
 
+  renderCount++;
   return (
     <div>
+      <h1>YouTube Form ({renderCount/2})</h1>
       <form>
         <label htmlFor="username">Username</label>
         <input
